5e21fcc149a027f6c56c04a52d5405e38395969b
Thecakeisgit <Thecakeisplaying@gmail.com>
2015-10-28 23:07:10 +0000

    A bit more code..

--- CubePrint.py
+++ CubePrint.py
@@ -1,16 +1,33 @@
 #!/usr/bin/python
  
-import BaseHTTPServer
-import CGIHTTPServer
 import os
- 
-PORT = 8888
-server_address = ("", PORT)
+import cherrypy
+import printrun
 
-server = BaseHTTPServer.HTTPServer
-handler = CGIHTTPServer.CGIHTTPRequestHandler
-handler.cgi_directories = [os.getcwd() + "htdocs/"]
-print "Serveur actif sur le port :", PORT
+tmp = str(os.popen("ls /dev/tty* | sed 's/[0-9]//g' | uniq").read())
+Serials = tmp.replace('\n', ' ')
+Serials = Serials.split(' ')
+print "[CubePrint]Identified Usables serials.. : " + str(Serials)
 
-httpd = server(server_address, handler)
-httpd.serve_forever()
+class CubePrint(object):
+    @cherrypy.expose
+    def index(self):
+        return """<!DOCTYPE html>
+	    <html>
+	        <head>	
+	            <title>CubePrint</title>
+		</head>
+		<body>
+
+		</body>
+	    </html>
+	"""
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 1234})
+
+cherrypy.config.update({'log.screen': False,
+                        'log.access_file': 'acceslog.log',
+                        'log.error_file': 'serverlog.log'})
+
+cherrypy.quickstart(CubePrint())
