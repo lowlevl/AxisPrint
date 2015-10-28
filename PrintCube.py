099f4db0c7a7f0babd00fb0590f544f5ea9f0721
Thecakeisgit <Thecakeisplaying@gmail.com>
2015-10-28 10:22:36 +0000

    INDEV: Innutile

--- PrintCube.py
+++ PrintCube.py
@@ -1,31 +1,3 @@
-#PrintCube Server 3D Printer server..
-
-import socket
-import sys
- 
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
- 
-#Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-print 'Socket bind complete'
- 
-#Start listening on socket
-s.listen(10)
-print 'Socket now listening on port :' + str(PORT)
- 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-s.close()
+#to send a file of gcode to the printer
+from printrun.printcore import printcore
+from printrun import gcoder
