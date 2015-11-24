0691dd734d85426ab53f8607775db36eda3d73a1
Thecakeisgit <Thecakeisplaying@gmail.com>
2015-11-24 13:10:53 +0000

    Progressively normal html form change to ajax requests

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,38 @@
-window.onload = function() {
-var textarea = document.getElementById('console');
-textarea.scrollTop = textarea.scrollHeight;
-};
+$(function(){ // JQuerry header
+    function scrollToBottom() { // Auto-Scroll the console
+      $('#Console').scrollTop($('#Console')[0].scrollHeight);
+    }
+    var IsChecked = false
+    var AutoScroll = null;
+
+    setInterval(function(){ // Setup a thread who update the console text
+        $.ajax({url: '/GetConsole'}).done(function(data) {
+            $("#Console").val(data['ConsoleText']);
+        });
+    }, 100);
+    
+	/* Console autodefil */
+    $('#AutoDefil').change(function(){ // Check for the "#AutoDefil" checkbox
+        IsChecked = $('#AutoDefil').is(':checked');
+        if (IsChecked){
+            AutoScroll = setInterval(function(){ scrollToBottom(); }, 100); // If the box is checked scroll down every 0.1 seconds
+        }else 
+        {
+            clearInterval(AutoScroll); // Else, stop the AutoScroll thread
+        }
+    });
+    /* End of console autodefil */
+	
+	/* Console module */
+    $('#ConsoleSender').unbind().click(function(){
+        $.ajax({
+			type: 'POST',
+			url: '/Console',
+			data: { 
+				'cmd': $('#CommandInput').val(), 
+			},
+			success: function(){}
+		});
+    });
+	/* End of Console module */
+});
