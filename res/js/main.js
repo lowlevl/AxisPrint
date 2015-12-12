47388597f2b2ced503e2fa266b5ab5f8e5b9dd7d
Thecakeisgit <Thecakeisplaying@gmail.com>
2015-12-12 10:14:50 +0000

    Styling and Scripting is cool !

--- res/js/main.js
+++ res/js/main.js
@@ -1,21 +1,21 @@
 $(function(){ // JQuerry header
-    function scrollToBottom() { // Auto-Scroll the console
-      $('#Console').scrollTop($('#Console')[0].scrollHeight);
-    }
-    var IsChecked = false
+
+    var IsChecked = $('#AutoDefil').is(':checked');
     var AutoScroll = null;
 
+	/* Console updater */
     setInterval(function(){ // Setup a thread who update the console text
         $.ajax({url: '/GetConsole'}).done(function(data) {
             $("#Console").val(data['ConsoleText']);
         });
-    }, 100);
+    }, 50);
+	/* End of console updater */
     
 	/* Console autodefil */
     $('#AutoDefil').change(function(){ // Check for the "#AutoDefil" checkbox
         IsChecked = $('#AutoDefil').is(':checked');
         if (IsChecked){
-            AutoScroll = setInterval(function(){ scrollToBottom(); }, 100); // If the box is checked scroll down every 0.1 seconds
+            AutoScroll = setInterval(function(){ $('#Console').scrollTop($('#Console')[0].scrollHeight); }, 50); // If the box is checked scroll down every 0.1 seconds
         }else 
         {
             clearInterval(AutoScroll); // Else, stop the AutoScroll thread
@@ -24,7 +24,7 @@ $(function(){ // JQuerry header
     /* End of console autodefil */
 	
 	/* Console module */
-    $('#ConsoleSender').unbind().click(function(){
+    $('#ConsoleSender').unbind('click').bind('click', function(){
         $.ajax({
 			type: 'POST',
 			url: '/Console',
@@ -35,4 +35,31 @@ $(function(){ // JQuerry header
 		});
     });
 	/* End of Console module */
+	
+	/* Connect module */
+	$('#ConnectRequest').unbind('click').bind('click', function(){
+        $.ajax({
+			type: 'POST',
+			url: '/ConnectPrinter',
+			data: { 
+				'_SerialPort': $('#SerialPort').val(),
+				'_BaudSpeed': $('#BaudSpeed').val()
+			},
+			success: function(){}
+		});
+    });
+	/* End of connect module */
+	
+	/* All links triggers */
+	$('#RfrshSer').click(function(){$.ajax({url: '/ReFreshSerials'});});
+	$('#DscnctPrtr').click(function(){$.ajax({url: '/DisconnectPrinter'});});
+	$('#RbootPi').click(function(){$.ajax({url: '/ReBootPi'});});
+	$('#ShutPi').click(function(){$.ajax({url: '/DownPi'});});
+	$('#StartPrt').click(function(){$.ajax({url: '/StartPrint'});});
+	$('#PausePrt').click(function(){$.ajax({url: '/PausePrint'});});
+	$('#CancelPrt').click(function(){$.ajax({url: '/CancelPrint'});});
+	$('#EmerStop').click(function(){$.ajax({url: '/EmergencyStop'});});
+	$('#ATXon').click(function(){$.ajax({url: '/ATXon'});});
+	$('#ATXoff').click(function(){$.ajax({url: '/ATXoff'});});
+	$('#ClrConsole').click(function(){$.ajax({url: '/ClearConsole'})});
 });
